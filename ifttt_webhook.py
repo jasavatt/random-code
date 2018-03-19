9cc2ff978e00be8579eac54c1124832125bff0f0
Alex Savattone <jasavatt@nps.edu>
2018-03-19 08:39:44 -0700

    add iftt module

---
+++ ifttt_webhook.py
@@ -0,0 +1,27 @@
+###############################################################################
+#
+# Trigger webhook on IFTTT
+#
+# Author: Alex Savattone
+#
+# Python 3.X
+#
+# Change Log:
+# 3-19-18 initial
+#
+###############################################################################
+
+import requests
+from sys import argv
+
+api_key = 'epBsMRdmppeL3QohvEhJL'
+event = 'alert'
+url = 'https://maker.ifttt.com/trigger/{}/with/key/{}'
+
+def hook(v1, v2, v3):
+    payload = {'value1': v1, 'value2': v2, 'value3': v3}
+    r = requests.post (url.format(event, api_key), json=payload)
+    pass
+
+if __name__ == '__main__':
+    hook(argv[1], argv[2], argv[3])
